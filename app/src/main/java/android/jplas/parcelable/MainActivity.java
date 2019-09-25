948613a6ee491decb37344914b6845834372c8d6
FauzanAr <faurinta12345@gmail.com>
2019-09-25 14:41:38 +0800

    Creating variable to input data from gui

--- app/src/main/java/android/jplas/parcelable/MainActivity.java
+++ app/src/main/java/android/jplas/parcelable/MainActivity.java
@@ -5,17 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText inputNama;
+    private EditText inputEmail;
+    private EditText inputNomor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inputNama = findViewById(R.id.nameText);
+        inputEmail = findViewById(R.id.emailText);
+        inputNomor = findViewById(R.id.numberText);
     }
 
 
     public void displayActivity(View view) {
+        String nama = inputNama.getText().toString();
+        String email = inputEmail.getText().toString();
+        int nomor = Integer.parseInt(inputNomor.getText().toString());
         Intent intent =  new Intent(this, DisplayActivity.class);
         startActivity(intent);
     }
