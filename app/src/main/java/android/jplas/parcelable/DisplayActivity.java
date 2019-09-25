2a97227eab9e4162c92237b54a4bf12e0bb42a33
FauzanAr <faurinta12345@gmail.com>
2019-09-25 14:55:54 +0800

    creating data reciever

--- app/src/main/java/android/jplas/parcelable/DisplayActivity.java
+++ app/src/main/java/android/jplas/parcelable/DisplayActivity.java
@@ -3,12 +3,27 @@ package android.jplas.parcelable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayActivity extends AppCompatActivity {
+    private TextView nameOutput;
+    private TextView emailOutput;
+    private TextView numberOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+
+        biodata bio = getIntent().getParcelableExtra("BIODATA");
+
+        nameOutput = findViewById(R.id.nameText);
+        emailOutput = findViewById(R.id.emailText);
+        numberOutput = findViewById(R.id.numberText);
+
+        nameOutput.setText(bio.getNama());
+        emailOutput.setText(bio.getEmail());
+        numberOutput.setText(""+bio.getNomor());
     }
+
 }
